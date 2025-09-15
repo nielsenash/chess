452f8113cb23e8676c5a0c549be3688fbac9a092
Ashley Nielsen <ashleynielse@gmail.com>
2025-09-15 16:37:58 -0600

    refactor BishopMoves

--- shared/src/main/java/chess/BishopMoves.java
+++ shared/src/main/java/chess/BishopMoves.java
@@ -5,49 +5,29 @@ import java.util.HashSet;
 
 public class BishopMoves implements MoveCollection{
 
+    HashSet<ChessMove> set = new HashSet<>();
+
+    public void calculateMoves(ChessBoard board, ChessPosition myPosition, int xChange, int yChange){
+        var x = myPosition.row()+xChange;
+        var y = myPosition.col()+yChange;
+        while (x >= 1 && y >= 1 && x <= 8 && y <= 8){
+            set.add(new ChessMove(myPosition, new ChessPosition(x,y), null));
+            if (board.getBoard()[x-1][y-1]!= null){
+                break;
+            }
+            x+=xChange;
+            y+=yChange;
+        }
+    }
+
     @Override
     public Collection<ChessMove> getPieceMoves(ChessBoard board, ChessPosition myPosition) {
-        var set = new HashSet<ChessMove>();
-        var x = myPosition.row()-1;
-        var y = myPosition.col()-1;
-        while (x >= 1 && y >= 1 && x <= 8 && y <= 8){
-            set.add(new ChessMove(myPosition, new ChessPosition(x,y), null));
-            if (board.getBoard()[x-1][y-1]!= null){
-                break;
-            }
-            x--;
-            y--;
-        }
-        x = myPosition.row()-1;
-        y = myPosition.col()+1;
-        while (x >= 1 && y >= 1 && x <= 8 && y <= 8){
-            set.add(new ChessMove(myPosition, new ChessPosition(x,y), null));
-            if (board.getBoard()[x-1][y-1]!= null){
-                break;
-            }
-            x--;
-            y++;
-        }
-        x = myPosition.row()+1;
-        y = myPosition.col()-1;
-        while (x >= 1 && y >= 1 && x <= 8 && y <= 8){
-            set.add(new ChessMove(myPosition, new ChessPosition(x,y), null));
-            if (board.getBoard()[x-1][y-1]!= null){
-                break;
-            }
-            x++;
-            y--;
-        }
-        x = myPosition.row()+1;
-        y = myPosition.col()+1;
-        while (x >= 1 && y >= 1 && x <= 8 && y <= 8){
-            set.add(new ChessMove(myPosition, new ChessPosition(x,y), null));
-            if (board.getBoard()[x-1][y-1]!= null){
-                break;
-            }
-            x++;
-            y++;
-        }
+
+        calculateMoves(board,myPosition, 1,1);
+        calculateMoves(board,myPosition, 1,-1);
+        calculateMoves(board,myPosition, -1,1);
+        calculateMoves(board,myPosition, -1,-1);
+
         return set;
     }
 }
