7925208f3e452ed2ad1562887c24739f0401cf62
Ashley Nielsen <ashleynielse@gmail.com>
2025-09-16 18:11:13 -0600

    final pawn tests

--- shared/src/main/java/chess/PawnMoves.java
+++ shared/src/main/java/chess/PawnMoves.java
@@ -5,10 +5,30 @@ import java.util.HashSet;
 
 import static chess.ChessGame.TeamColor.BLACK;
 import static chess.ChessGame.TeamColor.WHITE;
+import static chess.ChessPiece.PieceType.*;
 
 
 public class PawnMoves implements MoveCollection{
     HashSet<ChessMove> set = new HashSet<>();
+
+    public void addMoves(ChessMove move, ChessGame.TeamColor color){
+        if (move.endPosition().row() == 8 && color == WHITE){
+            set.add(new ChessMove(move.startPosition(), move.endPosition(), BISHOP));
+            set.add(new ChessMove(move.startPosition(), move.endPosition(), KNIGHT));
+            set.add(new ChessMove(move.startPosition(), move.endPosition(), ROOK));
+            set.add(new ChessMove(move.startPosition(), move.endPosition(), QUEEN));
+        }
+        else if (move.endPosition().row() == 1 && color == BLACK){
+            set.add(new ChessMove(move.startPosition(), move.endPosition(), BISHOP));
+            set.add(new ChessMove(move.startPosition(), move.endPosition(), KNIGHT));
+            set.add(new ChessMove(move.startPosition(), move.endPosition(), ROOK));
+            set.add(new ChessMove(move.startPosition(), move.endPosition(), QUEEN));
+        }
+        else{
+            set.add(move);
+        }
+
+    }
 
     public void addMovesForStartingSquare(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor color){
         int x = myPosition.row()-1;
@@ -18,8 +38,8 @@ public class PawnMoves implements MoveCollection{
         if (color == BLACK){
             change = -2;
         }
-        if (board.getBoard()[x+change][y] == null){
-            set.add(new ChessMove(myPosition, new ChessPosition(x+change+1,y+1), null));
+        if (board.getBoard()[x+change][y] == null && board.getBoard()[x+(change/2)][y] == null){
+            addMoves(new ChessMove(myPosition, new ChessPosition(x+change+1,y+1), null),color);
         }
     }
 
@@ -32,7 +52,29 @@ public class PawnMoves implements MoveCollection{
             change = -1;
         }
         if (x >= 1 && x <= 6 && board.getBoard()[x+change][y] == null){
-            set.add(new ChessMove(myPosition, new ChessPosition(x+change+1,y+1), null));
+            addMoves(new ChessMove(myPosition, new ChessPosition(x+change+1,y+1), null), color);
+        }
+    }
+
+    public void addDiagonalMoves(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor color){
+        int x = myPosition.row();
+        int y = myPosition.col();
+
+        if (y>0){
+            if (color == WHITE && board.getBoard()[x][y-2]!= null && board.getPiece(new ChessPosition(x+1, y-1)).pieceColor()==BLACK){
+                addMoves(new ChessMove(myPosition, new ChessPosition(x+1,y-1), null),color);
+            }
+            if (color == BLACK && board.getBoard()[x-2][y-2]!= null && board.getPiece(new ChessPosition(x-1, y-1)).pieceColor()==WHITE){
+                addMoves(new ChessMove(myPosition, new ChessPosition(x-1,y-1), null),color);
+            }
+        if (y<7){
+            if (color == WHITE && board.getBoard()[x][y]!= null && board.getPiece(new ChessPosition(x+1, y+1)).pieceColor()==BLACK){
+                addMoves(new ChessMove(myPosition, new ChessPosition(x+1,y+1), null),color);
+            }
+            if (color == BLACK && board.getBoard()[x-2][y]!= null && board.getPiece(new ChessPosition(x-1, y+1)).pieceColor()==WHITE){
+                addMoves(new ChessMove(myPosition, new ChessPosition(x-1,y+1), null),color);
+            }
+        }
         }
     }
 
@@ -42,6 +84,8 @@ public class PawnMoves implements MoveCollection{
         if ((myPosition.row() == 2 && color == WHITE) || (myPosition.row() == 7 && color == BLACK)){
             addMovesForStartingSquare(board, myPosition, color);
         }
+        addDiagonalMoves(board,myPosition,color);
+
         return set;
     }
 }
