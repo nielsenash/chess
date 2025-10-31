bb2127ff18d2ae8be1debfe32575fdad63fc94c5
Ashley Nielsen <ashleynielse@gmail.com>
2025-10-30 22:25:32 -0600

    add logic for getUser

--- server/src/main/java/dataaccess/SqlUserDataAccess.java
+++ server/src/main/java/dataaccess/SqlUserDataAccess.java
@@ -15,14 +15,16 @@ public class SqlUserDataAccess implements UserDataAccess {
     @Override
     public UserData saveUser(UserData user) throws DataAccessException {
         try (var conn = getConnection()) {
-            try (var preparedStatement = conn.prepareStatement(
-                    "INSERT INTO user (username, password, email) VALUES (?, ?, ?)")) {
+            if (getUser(user.username()) != null) {
+                try (var preparedStatement = conn.prepareStatement(
+                        "INSERT INTO user (username, password, email) VALUES (?, ?, ?)")) {
 
-                preparedStatement.setString(1, user.username());
-                preparedStatement.setString(2, user.password());
-                preparedStatement.setString(3, user.email());
+                    preparedStatement.setString(1, user.username());
+                    preparedStatement.setString(2, user.password());
+                    preparedStatement.setString(3, user.email());
 
-                System.out.println("Inserted user: " + preparedStatement.executeUpdate());
+                    System.out.println("Inserted user: " + preparedStatement.executeUpdate());
+                }
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -31,18 +33,30 @@ public class SqlUserDataAccess implements UserDataAccess {
     }
 
 
+    @Override
     public UserData getUser(String username) throws DataAccessException {
         UserData user = null;
+
         try (var conn = getConnection()) {
             try (var preparedStatement = conn.prepareStatement(
-                    "SELECT username, password, email FROM user WHERE username = username;")) {
+                    "SELECT username, password, email FROM user WHERE username = ?")) {
+                preparedStatement.setString(1, username);
+
                 var rs = preparedStatement.executeQuery();
-                rs.next();
-                System.out.println(rs.getInt(1));
+                if (rs.next()) {
+                    user = new UserData(
+                            rs.getString("username"),
+                            rs.getString("password"),
+                            rs.getString("email")
+                    );
+                }
+
             }
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new DataAccessException(e.getMessage());
         }
+
         return user;
     }
+
 }
