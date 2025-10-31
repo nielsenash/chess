3e4f185f76428d6f9a57c2308ab6f8bd6c2af0b4
Ashley Nielsen <ashleynielse@gmail.com>
2025-10-30 23:05:39 -0600

    save auth

--- server/src/main/java/dataaccess/SqlAuthDataAccess.java
+++ server/src/main/java/dataaccess/SqlAuthDataAccess.java
@@ -2,15 +2,37 @@ package dataaccess;
 
 import model.AuthData;
 
+import java.sql.SQLException;
+
+import static dataaccess.DatabaseManager.getConnection;
+
 public class SqlAuthDataAccess implements AuthDataAccess {
     @Override
-    public void clear() {
-
+    public void clear() throws DataAccessException {
+        try (var conn = getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(
+                    "TRUNCATE auth")) {
+                preparedStatement.executeUpdate();
+                System.out.println("Deleted auth table");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public void saveAuth(AuthData authData) {
+    public void saveAuth(AuthData authData) throws DataAccessException {
+        try (var conn = getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(
+                    "INSERT INTO auth (authToken, username) VALUES (?, ?, ?)")) {
+                preparedStatement.setString(1, authData.authToken());
+                preparedStatement.setString(2, authData.username());
 
+                System.out.println("Inserted auth: " + preparedStatement.executeUpdate());
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
