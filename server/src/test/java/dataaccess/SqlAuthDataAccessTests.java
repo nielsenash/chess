6f211017830b31cc8b6cc37a2043589af32025f6
Ashley Nielsen <ashleynielse@gmail.com>
2025-11-03 12:26:58 -0700

    save auth tests

--- server/src/test/java/dataaccess/SqlAuthDataAccessTests.java
+++ server/src/test/java/dataaccess/SqlAuthDataAccessTests.java
@@ -1,5 +1,6 @@
 package dataaccess;
 
+import model.AuthData;
 import model.UserData;
 import org.junit.jupiter.api.Test;
 
@@ -11,17 +12,19 @@ public class SqlAuthDataAccessTests {
     SqlAuthDataAccess sqlAuthDataAccess = new SqlAuthDataAccess();
     UserData user = new UserData("ashley", "nielsen", "jfa;8oeih");
     UserData user2 = new UserData(null, "nielsen", "jfa;8oeih");
+    AuthData auth = new AuthData(";aoiehtgb", ";aieht");
+    AuthData auth2 = new AuthData(null, ";aieht");
 
-//    @Test
-//    void saveAuth() throws Exception {
-//        assertNotNull(sqlUserDataAccess.saveUser(user));
-//    }
-//
-//   @Test
-//    void badSaveAuth() throws Exception {
-//        assertThrows(RuntimeException.class, () -> sqlUserDataAccess.saveUser(user2));
-//    }
-//
+    @Test
+    void saveAuth() throws Exception {
+        assertNotNull(sqlAuthDataAccess.saveAuth(auth));
+    }
+
+    @Test
+    void badSaveAuth() throws Exception {
+        assertThrows(RuntimeException.class, () -> sqlAuthDataAccess.saveAuth(auth2));
+    }
+
 //    @Test
 //    void getUser() throws Exception {
 //        sqlUserDataAccess.saveUser(user);
