787d3cff1e2576d8f89d87408e003c9089c86f27
Ashley Nielsen <ashleynielse@gmail.com>
2025-12-05 15:32:09 -0700

    create LoadGameMessage clas

---
+++ shared/src/main/java/websocket/messages/LoadGameMessage.java
@@ -0,0 +1,18 @@
+package websocket.messages;
+
+import chess.ChessGame;
+
+import static websocket.messages.ServerMessage.ServerMessageType.LOAD_GAME;
+
+public class LoadGameMessage extends ServerMessage {
+    private final ChessGame game;
+
+    public LoadGameMessage(ChessGame game) {
+        super(LOAD_GAME);
+        this.game = game;
+    }
+
+    public ChessGame getGame() {
+        return game;
+    }
+}
