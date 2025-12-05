46f19ff8eb47818b6ba3947bbb7d13533ad67bdf
Ashley Nielsen <ashleynielse@gmail.com>
2025-12-05 15:33:46 -0700

    create NotificationMessage

---
+++ shared/src/main/java/websocket/messages/NotificationMessage.java
@@ -0,0 +1,16 @@
+package websocket.messages;
+
+import static websocket.messages.ServerMessage.ServerMessageType.NOTIFICATION;
+
+public class NotificationMessage extends ServerMessage {
+    private final String message;
+
+    public NotificationMessage(String message) {
+        super(NOTIFICATION);
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
